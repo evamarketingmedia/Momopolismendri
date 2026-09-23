1fad1206519bcdd468bca18d133d08be506c6806
evamarketingmedia <evamarketingmedia@gmail.com>
2026-09-23 21:59:51 +0200

    Update layout.tsx

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -44,11 +44,16 @@ export async function generateMetadata({
       template: `%s · ${siteConfig.name}`,
     },
     description: dict.home.heroSubtitle,
-    icons: {
-      icon: [{ url: "/momopolis/logo-originale.webp", type: "image/webp" }],
-      shortcut: "/momopolis/logo-originale.webp",
-      apple: "/momopolis/logo-originale.webp",
+icons: {
+    icon: [
+      {
+      url: "/icon.png",
+      type: "image/png",
     },
+  ],
+  shortcut: "/icon.png",
+  apple: "/icon.png",
+},
     keywords:
       lang === "it"
         ? [
